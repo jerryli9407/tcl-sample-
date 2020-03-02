3e794908d4cd9ed77252060a8d8ba6d9d06a90ba
遥山螟 <61181966+jerryli9407@users.noreply.github.com>
2020-03-02 20:53:22 +0800

    Update continue.tcl

--- continue.tcl
+++ continue.tcl
@@ -1,15 +1,13 @@
 #output i =0 i=2 i=6 ,skip i=4
-
 for {set i 0} {$i < 10} {incr i 2} {
     if {$i == 4} {
-        continue;}
+        continue;}   # continue 忽略i=4时的循环体而去执行下一个循环体
     if {$i >6} {
-        break}
+        break}       #break 中指整个循环体，跳出当前for循环
         puts "i = $i"
 }
 
 #another order
-
 for {set i 0} {$i < 10} {incr i 2} {
     if {$i == 4} {
         continue;}
